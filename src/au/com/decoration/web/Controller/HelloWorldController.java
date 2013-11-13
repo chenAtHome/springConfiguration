38ec40d4e0fc35b1a3d4ca9d09e2b38303f4ca55
chenAtHome <chenchen@192-168-1-8.tpgi.com.au>
2013-11-13 21:14:41 +1100

    spring 1

---
+++ src/au/com/decoration/web/Controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package au.com.decoration.web.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class HelloWorldController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest arg0,
+			HttpServletResponse arg1) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("-----hello spring -----");
+		return new ModelAndView("/welcome");
+	}
+
+}
